3e5cd752a9c0fcfb13c876a90342ed1be17333b8
kawksung <86940335+kawkmin@users.noreply.github.com>
2023-10-14 16:35:17 +0900

    Revert "Revert "Feature/#15 채용공고 목록 조회 및 검색 기능 구현""

--- src/main/java/com/wanted/wantedpreonboardingbackend/domain/recruit/api/RecruitController.java
+++ src/main/java/com/wanted/wantedpreonboardingbackend/domain/recruit/api/RecruitController.java
@@ -5,17 +5,20 @@ import com.wanted.wantedpreonboardingbackend.domain.company.entity.Company;
 import com.wanted.wantedpreonboardingbackend.domain.recruit.application.RecruitService;
 import com.wanted.wantedpreonboardingbackend.domain.recruit.dto.request.RecruitCreateReqDto;
 import com.wanted.wantedpreonboardingbackend.domain.recruit.dto.request.RecruitUpdateReqDto;
+import com.wanted.wantedpreonboardingbackend.domain.recruit.dto.response.RecruitListResDto;
 import jakarta.validation.Valid;
 import java.net.URI;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -48,6 +51,20 @@ public class RecruitController {
   }
 
   /**
+   * 채용공고 검색 및 목록 조회
+   *
+   * @param search 검색명
+   * @return 200, 채용공고 목록
+   */
+  @GetMapping("")
+  public ResponseEntity<RecruitListResDto> getRecruits(
+      @RequestParam(required = false, defaultValue = "") String search
+  ) {
+    RecruitListResDto response = recruitService.getRecruits(search);
+    return ResponseEntity.status(HttpStatus.OK).body(response);
+  }
+
+  /**
    * 채용공고 수정
    *
    * @param companyId 회사 아이디
@@ -61,6 +78,7 @@ public class RecruitController {
       @PathVariable Long recruitId,
       @RequestBody @Valid RecruitUpdateReqDto reqDto
   ) {
+
     // 회사 id로 가져온 회사
     Company company = companyService.getCompanyById(companyId);
 
--- src/main/java/com/wanted/wantedpreonboardingbackend/domain/recruit/dao/RecruitRepository.java
+++ src/main/java/com/wanted/wantedpreonboardingbackend/domain/recruit/dao/RecruitRepository.java
@@ -1,8 +1,30 @@
 package com.wanted.wantedpreonboardingbackend.domain.recruit.dao;
 
 import com.wanted.wantedpreonboardingbackend.domain.recruit.entity.Recruit;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface RecruitRepository extends JpaRepository<Recruit, Long> {
+
+  /**
+   * 검색명에 하나라도 일치
+   *
+   * @param search 검색명
+   * @return 조건에 맞는 채용공고 리스트
+   */
+  @Query(
+      "SELECT r " +
+          "FROM Recruit r " +
+          "join fetch r.company " +
+          "WHERE lower(r.content) LIKE lower(concat('%',:search,'%') )" +
+          "OR lower(r.position) LIKE lower(concat('%',:search,'%') )" +
+          "OR lower(r.skill) LIKE lower(concat('%',:search,'%') )" +
+          "OR lower(r.company.name) LIKE lower(concat('%',:search,'%') )" +
+          "OR lower(r.company.region) LIKE lower(concat('%',:search,'%') )" +
+          "OR lower(r.company.country) LIKE lower(concat('%',:search,'%') )"
+  )
+  List<Recruit> searchRecruits(@Param("search") String search);
 
 }
